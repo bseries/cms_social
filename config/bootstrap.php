e6461c516c5428306138a62bcd9491286e47ad39
David Persson <davidpersson@gmx.de>
2014-01-21 20:47:13 +0100

    Adding docs for getting instagram settings.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -51,6 +51,22 @@ Settings::register('cms_social', 'service.twitter.default.consumerKey');
 Settings::register('cms_social', 'service.twitter.default.consumerSecret');
 Settings::register('cms_social', 'service.twitter.default.accessToken');
 Settings::register('cms_social', 'service.twitter.default.accessTokenSecret');
+
+// Instagram Settings
+//
+// - How to get your Instagram Access Token -
+//
+//   1. Create app in Instagram developer interface.
+//   2. As the redirect-URL use a non existent one on your website.
+//   3. Open the following URL in your browser and note down the "code":
+//      https://api.instagram.com/oauth/authorize/?client_id=[clientID]&redirect_uri=[redirectURI]&response_type=code
+//   4. Issue the following command to get the access token:
+//      curl -F 'client_id=[your_client_id]' -F 'client_secret=[your_secret_key]' -F 'grant_type=authorization_code' -F 'redirect_uri=[redirect_url]' -F 'code=[code]' https://api.instagram.com/oauth/access_token
+//
+// - How to get your Instagram user id -
+//
+// http://jelled.com/instagram/lookup-user-id
+//
 Settings::register('cms_social', 'service.instagram.default.username');
 Settings::register('cms_social', 'service.instagram.default.userId');
 Settings::register('cms_social', 'service.instagram.default.accessToken');
