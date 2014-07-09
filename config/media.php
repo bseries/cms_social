ee3a241fca49dc1dd076e0f6b850512f83e53eb2
David Persson <davidpersson@gmx.de>
2014-07-09 12:05:09 +0200

    Improve make handler.

--- config/media.php
+++ config/media.php
@@ -13,6 +13,8 @@
 use cms_media\models\Media;
 use cms_media\models\MediaVersions;
 use cms_social\models\Vimeo;
+use mm\Mime\Type;
+use Exception;
 
 // Registers Media and MediaVersions schemes.
 
@@ -24,22 +26,28 @@ Media::registerScheme('vimeo', [
 ]);
 
 // Uses Vimeo's thumbnail and generates our local versions off it. Will
-// not store/link versions for the video files themselves as  those cannot
-// be reached through the Vimeo API.
+// not store/link versions for the video files themselves as those cannot
+// be reached through the Vimeo API. This handler doesn't actuall make the
+// files itself but uses a generic file make handler to do so.
 MediaVersions::registerScheme('vimeo', [
 	'make' => function($entity) {
-		// @fixme Do not hardcode this. Check if convert target is
-		// image mime type instead.
-		if (strpos($entity->version, 'fix') === false) {
+		$version = MediaVersions::assembly($entity->name(), $entity->version);
+
+		// This handler only makes images. We don't want to download videos.
+		// Leave this version untoudched and just store the vimeo link.
+		if (!isset($version['convert']) || Type::guessName($version['convert']) !== 'image') {
 			return null; // Indicate skip.
 		}
+
 		// This changes the scheme of the entity, thus
 		// it capabilities.
 		$video = Vimeo::first(str_replace('vimeo://', '', $entity->url));
 		$entity->url = $video->thumbnail_large;
 
 		if (!$entity->can('download')) {
-			return null;
+			$message  = "Can't download vimeo poster URL `{$entity->url}`. ";
+			$message .= "You need to register a http scheme with downloading enabled to do so.";
+			throw new Exception($message);
 		}
 		$entity->url = $entity->download();
 
