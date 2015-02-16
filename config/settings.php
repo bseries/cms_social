a2e93b4538da4d41e586246688ce5c88638c6985
David Persson <davidpersson@gmx.de>
2015-02-16 14:06:09 +0100

    Extract settings.

--- config/settings.php
+++ config/settings.php
@@ -12,54 +12,36 @@
 
 use base_core\extensions\cms\Settings;
 
+// Two settings for each social service are available:
+//
+// 1. autopublish, allows to automatically publish new
+//    entities in stream.
+// 2. stream, allows to in or exclude service from
+//    social stream.
+
 Settings::register('service.tumblr.default', [
 	'autopublish' => false,
-	'username' => null
-]);
+	'stream' => false
+] + Settings::read('service.tumblr.default'));
+
 Settings::register('service.vimeo.default', [
 	'autopublish' => false,
-	'username' => null
-]);
+	'stream' => false
+] + Settings::read('service.vimeo.default'));
+
 Settings::register('service.facebook.default', [
 	'autopublish' => false,
-	'appId' => null,
-	'appSecret' => null,
-	'pageUrl' => null
-]);
+	'stream' => false
+] + Settings::read('service.vimeo.default'));
+
 Settings::register('service.twitter.default', [
 	'autopublish' => false,
-	'username' => null,
-	'consumerKey' => null,
-	'accessToken' => null,
-	'accessTokenSecret' => null
-]);
+	'stream' => false
+] + Settings::read('service.twitter.default'));
 
-// Instagram Settings
-//
-// - How to get your Instagram Access Token -
-//
-//   1. Create app in Instagram developer interface.
-//   2. As the redirect-URL use a non existent one on your website.
-//   3. Open the following URL in your browser and note down the "code":
-//      https://api.instagram.com/oauth/authorize/?client_id=[client_id]&redirect_uri=[redirect_uri]&response_type=code
-//   4. Issue the following command to get the access token:
-//      curl \
-//        -F 'client_id=[your_client_id]' \
-//        -F 'client_secret=[your_secret_key]' \
-//        -F 'grant_type=authorization_code' \
-//        -F 'redirect_uri=[redirect_url]' \
-//        -F 'code=[code]' \
-//        https://api.instagram.com/oauth/access_token; echo ''
-
-// - How to get your Instagram user id -
-//
-// http://jelled.com/instagram/lookup-user-id
-//
 Settings::register('service.instagram.default', [
 	'autopublish' => false,
-	'username' => null,
-	'userId' => null,
-	'accessToken' => null
-]);
+	'stream' => false
+] + Settings::read('service.twitter.default'));
 
 ?>
